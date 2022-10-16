359ae2377019a1bb6e2fec54c82286654d123efa
xdsolraC <xds0lr4c@gmail.com>
2022-10-16 01:06:22 +0000

    Awaken bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/voltage_raphael.mk
+    $(LOCAL_DIR)/awaken_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_raphael-user \
-    voltage_raphael-userdebug \
-    voltage_raphael-eng
+    awaken_raphael-user \
+    awaken_raphael-userdebug \
+    awaken_raphael-eng
